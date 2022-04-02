2043bddbcbb2db2fd67c66dd77fe29cbefae736b
Jola Ajayi <benjamen.ajayi@gmail.com>
2022-04-02 15:42:57 -0400

    Made the left and right ear labels into buttons, finished 4. Connection test

--- build-COMP-3004-Final-Project-Team-30-Desktop-Debug/ui_mainwindow.h
+++ build-COMP-3004-Final-Project-Team-30-Desktop-Debug/ui_mainwindow.h
@@ -32,8 +32,6 @@ public:
     QPushButton *downButton;
     QPushButton *tickButton;
     QLabel *intensityLabel;
-    QLabel *leftEarLabel;
-    QLabel *rightEarLabel;
     QLabel *leftConnectionLabel;
     QLabel *twentyMinsLabel;
     QLabel *customSessionLabel;
@@ -61,6 +59,9 @@ public:
     QLabel *rightConnectionLabel;
     QLabel *thetaLabel;
     QLabel *powerLabel;
+    QPushButton *leftEarButton;
+    QPushButton *rightEarButton;
+    QPushButton *connectionTestButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -68,7 +69,7 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(806, 566);
         QIcon icon;
         icon.addFile(QString::fromUtf8(":/images/overlay/OasisProSkeleton.svg"), QSize(), QIcon::Normal, QIcon::Off);
         MainWindow->setWindowIcon(icon);
@@ -107,14 +108,6 @@ public:
         font.setPointSize(25);
         intensityLabel->setFont(font);
         intensityLabel->setStyleSheet(QString::fromUtf8("color:yellow"));
-        leftEarLabel = new QLabel(centralwidget);
-        leftEarLabel->setObjectName(QString::fromUtf8("leftEarLabel"));
-        leftEarLabel->setGeometry(QRect(440, 130, 41, 21));
-        leftEarLabel->setStyleSheet(QString::fromUtf8("border-image:url(:/images/icons/LeftOff.svg)"));
-        rightEarLabel = new QLabel(centralwidget);
-        rightEarLabel->setObjectName(QString::fromUtf8("rightEarLabel"));
-        rightEarLabel->setGeometry(QRect(500, 130, 41, 21));
-        rightEarLabel->setStyleSheet(QString::fromUtf8("border-image:url(:/images/icons/RightOff.svg)"));
         leftConnectionLabel = new QLabel(centralwidget);
         leftConnectionLabel->setObjectName(QString::fromUtf8("leftConnectionLabel"));
         leftConnectionLabel->setGeometry(QRect(450, 110, 31, 16));
@@ -252,10 +245,25 @@ public:
         powerLabel->setGeometry(QRect(520, 380, 41, 16));
         powerLabel->setStyleSheet(QString::fromUtf8("border-image:url(:/images/icons/powerOff.svg)\n"
 ""));
+        leftEarButton = new QPushButton(centralwidget);
+        leftEarButton->setObjectName(QString::fromUtf8("leftEarButton"));
+        leftEarButton->setGeometry(QRect(440, 130, 41, 21));
+        leftEarButton->setStyleSheet(QString::fromUtf8("border-image:url(:/images/icons/LeftOff.svg)\n"
+"\n"
+""));
+        rightEarButton = new QPushButton(centralwidget);
+        rightEarButton->setObjectName(QString::fromUtf8("rightEarButton"));
+        rightEarButton->setGeometry(QRect(500, 130, 41, 21));
+        rightEarButton->setStyleSheet(QString::fromUtf8("border-image:url(:/images/icons/RightOff.svg)\n"
+"\n"
+""));
+        connectionTestButton = new QPushButton(centralwidget);
+        connectionTestButton->setObjectName(QString::fromUtf8("connectionTestButton"));
+        connectionTestButton->setGeometry(QRect(20, 10, 131, 25));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 22));
+        menubar->setGeometry(QRect(0, 0, 806, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -274,8 +282,6 @@ public:
         downButton->setText(QString());
         tickButton->setText(QString());
         intensityLabel->setText(QApplication::translate("MainWindow", "INT", nullptr));
-        leftEarLabel->setText(QString());
-        rightEarLabel->setText(QString());
         leftConnectionLabel->setText(QString());
         twentyMinsLabel->setText(QString());
         customSessionLabel->setText(QString());
@@ -303,6 +309,9 @@ public:
         rightConnectionLabel->setText(QString());
         thetaLabel->setText(QString());
         powerLabel->setText(QString());
+        leftEarButton->setText(QString());
+        rightEarButton->setText(QString());
+        connectionTestButton->setText(QApplication::translate("MainWindow", "connection test", nullptr));
     } // retranslateUi
 
 };
